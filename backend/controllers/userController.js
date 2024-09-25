31dd52a011b673f201db97a7ae3ea7f0950a0492
Hugo Melin <hugo.melin01@gmail.com>
2024-09-25 23:02:19 +0200

    End refactoring by adding error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
@@ -6,11 +7,16 @@ const { sendVerificationEmail } = require('../services/emailsService');
 exports.createUser = async (req, res) => {
   try {
     const userData = req.body;
+    if (!userData.email || !userData.password || !userData.username) {
+      res.status(400).json({ message: 'Email, mot de passe et nom d\'utilisateur sont requis' });
+    }
+
     userData.password = await bcrypt.hash(userData.password, 10);
     userData.verificationToken = await sendVerificationEmail(userData.email);
     const newUser = await User.create(userData);
     res.status(201).json({ message: 'Utilisateur créé avec succès.', user: newUser });
   } catch (error) {
+    console.error(`Erreur lors de la création de l'utilisateur: ${error}`);
     res.status(500).json({ message: error.message });
   }
 };
@@ -18,8 +24,13 @@ exports.createUser = async (req, res) => {
 exports.getAllUsers = async (req, res) => {
   try {
     const users = await User.findAll();
+    if (users.length === 0) {
+      console.error('Aucun utilisateur n\'a été trouvé');
+      res.status(404).json({ message: 'Aucun utilisateur n\'a été trouvé' });
+    }
     res.status(200).json(users);
   } catch (error) {
+    console.error(`Erreur lors de la récupération des utilisateurs: ${error}`);
     res.status(500).json({ message: error.message });
   }
 };
@@ -28,8 +39,13 @@ exports.getOneUserById = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await User.findById(id);
+    if (!user) {
+      console.error('Aucun utilisateur trouvé pour cette Id');
+      res.status(404).json({ message: 'Aucun utilisateur trouvé' });
+    }
     res.status(200).json(user);
   } catch (error) {
+    console.error(`Erreur lors de la récupération de l'utilisateur: ${error}`);
     res.status(500).json({ message: error.message });
   }
 };
@@ -41,8 +57,12 @@ exports.deleteUser = async (req, res) => {
     if (userToDelete) {
       await User.delete(userToDelete);
       res.status(200).json({ message: 'User successfully delete', userDeleted: userToDelete });
+    } else {
+      console.error('Aucun utilisateur trouvé pour cette Id');
+      res.status(404).json({ message: 'Aucun utilisateur à supprimer' });
     }
   } catch (error) {
+    console.error(`Erreur lors de la suppression de l'utilisateur: ${error}`);
     res.status(500).json({ message: error.message });
   }
 };
@@ -62,9 +82,11 @@ exports.updateUser = async (req, res) => {
       const updatedUser = await User.findById(id);
       res.status(200).json({ message: 'User successfully updated', userUpdated: updatedUser });
     } else {
-      res.status(404).json('No user found');
+      console.error('Aucun utilisateur trouvé pour cette Id');
+      res.status(404).json({ message: 'No user found' });
     }
   } catch (error) {
+    console.error(`Erreur lors de la modification de l'utilisateur: ${error}`);
     res.status(500).json({ message: error.message });
   }
 };
@@ -78,6 +100,7 @@ exports.verifyUser = async (req, res) => {
     if (error.message === 'Token de vérification invalide ou déjà utilisé') {
       res.status(400).send(error.message);
     } else {
+      console.error(`Erreur lors de la vérification de l'utilisateur: ${error}`);
       res.status(500).send(error.message);
     }
   }
