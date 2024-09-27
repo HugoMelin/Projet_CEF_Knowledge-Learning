ab7c7fcec63541e8aaebfbe4810bd75f627149cb
Hugo Melin <hugo.melin01@gmail.com>
2024-09-27 14:54:30 +0200

    Add sercure routes function

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+const secure = require('../middleware/secure');
 
 router.post('/', userController.createUser);
 
