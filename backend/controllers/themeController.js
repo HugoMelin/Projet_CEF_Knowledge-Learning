3d7653827c72cae89296b5810fade65d84fdce3f
Hugo Melin <hugo.melin01@gmail.com>
2024-09-28 14:22:56 +0200

    Add full crud for themes table

--- backend/controllers/themeController.js
+++ backend/controllers/themeController.js
@@ -34,3 +34,60 @@ exports.getAllThemes = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.getOneThemeById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const theme = await Theme.findById(id);
+    if (!theme) {
+      console.error('Aucun thème trouvé pour cette Id');
+      res.status(404).json({ message: 'Aucun thème trouvé' });
+    }
+    res.status(200).json(theme);
+  } catch (error) {
+    console.error(`Erreur lors de la récupération du thème: ${error}`);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteTheme = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const themeToDelete = await Theme.findById(id);
+    if (themeToDelete) {
+      await Theme.delete(themeToDelete);
+      res.status(200).json({ message: 'Theme successfully delete', themeDeleted: themeToDelete });
+    } else {
+      console.error('Aucun thème trouvé pour cette Id');
+      res.status(404).json({ message: 'Aucun thème à supprimer' });
+    }
+  } catch (error) {
+    console.error(`Erreur lors de la suppression du thème: ${error}`);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateTheme = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const themeData = req.body;
+
+    if (themeData.name) {
+      const themeToUpdate = await Theme.findById(id);
+      if (themeToUpdate) {
+        await Theme.update(themeToUpdate, themeData);
+        const updatedTheme = await Theme.findById(id);
+        res.status(200).json({ message: 'Theme successfully updated', themeUpdated: updatedTheme });
+      } else {
+        console.error('Aucun thème trouvé pour cette Id');
+        res.status(404).json({ message: 'Aucun thème trouvé pour cette Id' });
+      }
+    } else {
+      console.error('Vous devez donner un nouveau nom au thème');
+      res.status(500).json({ message: 'Vous devez donner un nouveau nom au thème' });
+    }
+  } catch (error) {
+    console.error(`Erreur lors de la modification du theme: ${error}`);
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/models/Theme.js
+++ backend/models/Theme.js
@@ -38,6 +38,17 @@ class Theme {
     }
   }
 
+  static async findById(id) {
+    try {
+      const [rows] = await db.query('SELECT name, id_themes FROM themes WHERE id_themes = ?', [id]);
+      const result = rows.map((row) => new Theme(...Object.values(row)));
+      return result[0];
+    } catch (error) {
+      console.error(`Erreur lors de la recherche d'un thème par ID: ${error}`);
+      throw error;
+    }
+  }
+
   static async findByName(name) {
     try {
       const [rows] = await db.query('SELECT name, id_themes FROM themes WHERE name = ?', [name]);
@@ -45,6 +56,51 @@ class Theme {
       return result[0];
     } catch (error) {
       console.error(`Erreur lors de la recherche d'un thème par nom': ${error}`);
+      throw error;
+    }
+  }
+
+  static async delete(data) {
+    try {
+      const [response] = await db.query(`
+        DELETE 
+        FROM themes 
+        WHERE id_themes = ?
+      `, [data.idThemes]);
+
+      if (response.affectedRows === 0) {
+        console.error('Aucun thème n\'a été supprimé');
+        throw new Error('Aucun thème n\'a été supprimé');
+      }
+
+      return response;
+    } catch (error) {
+      console.error(`Erreur lors de la suppression du thème: ${error}`);
+      throw error;
+    }
+  }
+
+  static async update(theme, dataToUpdate) {
+    try {
+      const {
+        name,
+      } = dataToUpdate;
+
+      const [response] = await db.query(`
+        UPDATE themes 
+        SET 
+          name = COALESCE(?, name)
+        WHERE id_themes = ?;
+        `, [name, theme.idThemes]);
+
+      if (response.affectedRows === 0) {
+        console.error('Aucun thème n\'a été mis à jour');
+        throw new Error('Aucun thème n\'a été mis à jour');
+      }
+
+      return response;
+    } catch (error) {
+      console.error(`Erreur lors de la mise à jour du thème: ${error}`);
       throw error;
     }
   }
